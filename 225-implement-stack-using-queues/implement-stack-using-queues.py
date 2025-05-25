87c89af26b750b7719b4c4edbb679f1adc6f9689
Saransh Gupta <143245292+SaranshG2501@users.noreply.github.com>
2025-05-25 18:44:25 +0530

    Time: 1 ms (2.15%) | Memory: 12.4 MB (55.17%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+class MyStack(object):
+
+    def __init__(self):
+        self.queue1 = deque()  # Main queue to hold stack elements
+        self.queue2 = deque()  # Auxiliary queue for operations
+
+    def push(self, x):
+        """
+        Pushes element x to the top of the stack.
+        """
+        # Add the new element to the auxiliary queue
+        self.queue2.append(x)
+        
+        # Move all elements from queue1 to queue2
+        while self.queue1:
+            self.queue2.append(self.queue1.popleft())
+        
+        # Swap the names of the two queues
+        self.queue1, self.queue2 = self.queue2, self.queue1
+
+    def pop(self):
+        """
+        Removes the element on the top of the stack and returns it.
+        """
+        return self.queue1.popleft()  # The front of queue1 is the top of the stack
+
+    def top(self):
+        """
+        Returns the element on the top of the stack.
+        """
+        return self.queue1[0]  # The front of queue1 is the top of the stack
+
+    def empty(self):
+        """
+        Returns true if the stack is empty, false otherwise.
+        """
+        return not self.queue1  # Check if queue1 is empty
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
